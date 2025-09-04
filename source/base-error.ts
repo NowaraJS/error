6da80c420cd5ed2e9c2e344807822de7b94958c6
Komiroko <komiriko@pm.me>
2025-09-04 15:18:52 +0200

    feat(🚀): [Add typed causes in BaseError and HttpError] ## Features - Introduced examples demonstrating the usage of generic typing with `BaseError` and `HttpError`. - Added various scenarios showcasing type safety with different cause types, including objects, strings, and union types.
    ## Description
    This commit adds a new file `typed-cause.ts` that provides practical examples of how to utilize the generic typing feature in the `BaseError` and `HttpError` classes. The examples illustrate how TypeScript can enforce type safety when handling error causes, enhancing the robustness of error handling in applications.

--- source/base-error.ts
+++ source/base-error.ts
@@ -1,8 +1,11 @@
-export class BaseError extends Error {
+export class BaseError<const TCause = unknown> extends Error {
+	public override readonly cause: TCause | undefined;
+
 	public readonly date: Date = new Date();
 
-	public constructor(message: string, cause?: unknown) {
+	public constructor(message: string, cause?: TCause) {
 		super(message, { cause });
+		this.cause = cause;
 		this.name = new.target.name;
 
 		if (Error.captureStackTrace)
